50bdfe1be486c4a5499b045cb5b4d7b4e4946629
sharon <kiokodiana4@gmail.com>
2024-08-15 10:37:54 +0300

    today

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     </div>
 
       <div className="h-20 mt-14 md:w-56 w-full bg-gradient-to-r from-orange-600 to-blue-600 flex flex-row items-center">
-        <Flower className="ml-5 text-purple-600 border rounded-full bg-white p-1" />            
+        <Flower className="ml-8 text-purple-600 w-8 h-8 border rounded-full bg-white p-1" />            
         <div className="m-2">
         <p className=" text-white text-sm">Lowest price guarantee</p>
         <p className=" text-white text-sm">Found Cheaper ones...</p>
